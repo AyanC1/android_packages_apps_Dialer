db04f5e37b65cc02a36253e4df702e6d1d2b900e
AyanC1 <choudhury.ayan13@gmail.com>
2021-05-09 16:27:09 +0200

    Revert "Dialer: Move to SPN schema classes"
    This reverts commit 2df84520c1adddbb99f513426038cf3599980909.

--- java/com/android/dialer/helplines/HelplineAdapter.java
+++ java/com/android/dialer/helplines/HelplineAdapter.java
@@ -94,8 +94,8 @@ class HelplineAdapter extends RecyclerView.Adapter<HelplineAdapter.ViewHolder> {
 
         @Override
         public boolean areItemsTheSame(int iOld, int iNew) {
-            String oldNumber = mOldList.get(iOld).getItem().getNumber();
-            String newNumber = mOldList.get(iNew).getItem().getNumber();
+            String oldNumber = mOldList.get(iOld).get("number");
+            String newNumber = mOldList.get(iNew).get("number");
             return oldNumber.equals(newNumber);
         }
 
@@ -146,7 +146,7 @@ class HelplineAdapter extends RecyclerView.Adapter<HelplineAdapter.ViewHolder> {
                 mLanguageView.setText(languages);
             }
 
-            String number = item.getItem().getNumber();
+            String number = item.get("number");
             if (!TextUtils.isEmpty(number)) {
                 mCallIcon.setVisibility(View.VISIBLE);
                 mCallIcon.setOnClickListener(v -> {
